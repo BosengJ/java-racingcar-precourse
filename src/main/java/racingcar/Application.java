cd227fe9845ab7a9c3c232b9121ac794bde222db
BosengJ <boseungee22@gmail.com>
2021-10-15 17:42:26 +0900

    input - car name

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Application {
 	public static void main(String[] args) {			
 		final Scanner scanner = new Scanner(System.in);
+		
 		// TODO 구현 진행
+		
+		System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+		String input = scanner.nextLine();
+		String[] carArray = input.split(",");
+		System.out.println();
+//		System.out.println(Arrays.toString(carArray));
 	}
 }
