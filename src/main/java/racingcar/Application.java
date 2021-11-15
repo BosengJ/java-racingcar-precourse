60fba77982b150b185c2273b968f7d7c7fad6a5e
BosengJ <boseungee22@gmail.com>
2021-11-15 14:14:31 +0900

    Application.java - CarList 생성하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Application {
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
+		String[] CarList = Game.generateCarList(scanner);
 		// TODO 구현 진행
 	}
 }
