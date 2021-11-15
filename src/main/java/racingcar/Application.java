8124d66ae7c2385b11891e5ecd39aaa6d175b0d0
BosengJ <boseungee22@gmail.com>
2021-11-15 17:06:34 +0900

    출력 조건에 "실행결과" text 넣어줘야 함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,20 @@
 package racingcar;
 
 import java.util.Scanner;
-import java.util.Arrays;
+//import java.util.Arrays;
 
 public class Application {
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
 		String[] CarList = Game.generateCarList(scanner);
 		int playNum = Game.playGameNtimes(scanner);
-		
+		System.out.println("\n실행결과");
+
 //		input test
 //		System.out.println(Arrays.toString(CarList));
 //		System.out.println(playNum);
 
 		// 기능 구현 목록
-		// Number of Game Plays
 		// Play Game (Print Result)
 	}
 }
