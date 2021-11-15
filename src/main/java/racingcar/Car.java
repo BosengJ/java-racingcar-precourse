770ede90cb79a5d7c17bbee7ed0885657980a3a1
BosengJ <boseungee22@gmail.com>
2021-11-15 17:27:33 +0900

    car.java - distanceRun
    랜덤한 숫자를 받아 4이상일 경우 position을 +1 해준다

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import utils.RandomUtils;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -9,4 +11,13 @@ public class Car {
     }
 
     // 추가 기능 구현
+    // Express the distance as "-"
+    // Express the distance as Number
+
+    public void distanceRun(){
+        int randomNum = utils.RandomUtils.nextInt(0,9);
+        if(randomNum >= 4){
+            ++position;
+        }
+    }
 }
