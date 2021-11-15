9ad8feb68d69e4c07a1bba580d17424c368ee173
BosengJ <boseungee22@gmail.com>
2021-11-15 14:26:17 +0900

    다음번에 할 기능 구현 목록 리스트 적어둠

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,8 @@ public class Application {
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
 		String[] CarList = Game.generateCarList(scanner);
-		// TODO 구현 진행
+		// 기능 구현 목록
+		// Number of Game Plays
+		// Play Game (Print Result)
 	}
 }
