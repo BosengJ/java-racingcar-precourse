d00859341070645346ee65cd3bb892f5685265f9
BosengJ <boseungee22@gmail.com>
2021-11-15 17:47:54 +0900

    car.java - PrintRunningImage 함수
    position 값에 따라 "-"로 표현해주는 함수

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -10,14 +10,23 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
-    // Express the distance as "-"
-    // Express the distance as Number
-
     public void distanceRun(){
         int randomNum = utils.RandomUtils.nextInt(0,9);
         if(randomNum >= 4){
             ++position;
         }
     }
+    // print the moving image
+    public void PrintRunningImage(){
+        String img = "";
+        for (int i=0; i < position; i++){
+            img += "-";
+        }
+        System.out.println(name + ":" + img);
+    }
+
+
+    // 추가 기능 구현
+    // Express the distance as "-"
+    // Express the distance as Number
 }
