134bb0880aed7daba9d506d26a820ffd83abb4df
BosengJ <boseungee22@gmail.com>
2021-11-15 13:51:55 +0900

    application 파일 초기화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,81 +1,10 @@
 package racingcar;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
-	public static void main(String[] args) {			
+	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
-
 		// TODO 구현 진행
-
-		boolean checkInput = true;
-		boolean checkCar = true;
-		boolean checkN = true;
-		String[] carArray = new String[0];
-		String n = "";
-
-		while(checkInput) {
-			// input 1) 경주할 자동차 이름을 쉼표를 기준으로 입력 받는다
-			System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
-			String input1 = scanner.nextLine();
-			carArray = input1.split(",");
-			System.out.println();
-
-			// input 2) 시도할 횟수를 입력 받는다
-			System.out.println("시도할 회수는 몇회인가요?");
-			String input2 = scanner.nextLine();
-			n = input2;
-
-			// Error 1) 자동차 이름이 5자 초과하는 경우
-			for (int i = 0; i < carArray.length; i++) {
-				if (carArray[i].length() == 0 || carArray[i].length() > 5) {
-					System.out.println("[ERROR] 자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
-					checkCar = false;
-				}
-			}
-
-			// Error 2) 시도횟수 입력시, 숫자가 아닌 다른 문자열을 입력했을 경우
-			for (int i = 0; i < n.length(); i++) {
-				char num = n.charAt(i);
-				int asccode = (int) num;
-				if (asccode < 48 || asccode > 57) {
-					System.out.println("[ERROR] 시도 횟수는 숫자여야 합니다.");
-					checkN = false;
-					break;
-				}
-			}
-
-			// Input에 error가 없을 시, while문 빠져나간다
-			if (checkCar == true && checkN == true) {
-				checkInput = false;
-			}
-		}
-
-		// check) carArray, n
-		System.out.println("carArray: " + Arrays.toString(carArray));
-		System.out.println("n: " + n);
-
-		// test
-		int randomNumber = utils.RandomUtils.nextInt(0, 9);
-		System.out.println("randomNUM: " + randomNumber);
-
-		// carArray의 element 하나씩 받아 car 이름 선언해주고, random 값 지정해주기
-		int [] randomArray;
-		randomArray = new int[carArray.length];
-		for (int i=0; i<randomArray.length; i++) {
-			int randomNumber = utils.RandomUtils.nextInt(0, 9);
-			randomArray[i] = randomNumber;
-		}
-		System.out.println("randomArray: " + Arrays.toString(randomArray));
-
-//		Car[] c = new Car[carArray.length]; // carArray의 element 개수만큼, 주소값을 저장할 메모리를 할당해준다
-//
-//		for (int i=0; i<carArray.length; i++) {
-//			c[i] = new Car(carArray[i]);
-//			c[i].printCarName();
-//			System.out.println("carName: " + carName);
-//			System.out.println(carArray[i]);
-		}
 	}
 }
