2572e64b82bbae017120330b890ad8c33eaf049d
BosengJ <boseungee22@gmail.com>
2021-11-15 14:24:59 +0900

    Game.java - checkInputData
    자동차 이름이 5를 초과하면 while문을 반복해서 input 값을 계속 받게 한다

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -11,11 +11,21 @@ public class Game {
 			String inputData = scanner.nextLine();
 			CarList = inputData.split(",");
 			// 받은 이름 5자 이하인지 확인하고 맞으면 플래그 바꿔주고 아니면 그대로 플래그 false
+			flag = checkInputData(CarList);
 			if(flag){
 				System.out.println("[ERROR] 자동차의 이름은 5자 이하여야 한다.");
 			}
 		}
 		return CarList;
+	}
 
+	public static boolean checkInputData(String[] carList){
+		boolean overfive = false;
+		for(int i=0; i<carList.length; i++){
+			if(carList[i].length() > 5){
+				overfive = true;
+			}
+		}
+		return overfive;
 	}
 }
