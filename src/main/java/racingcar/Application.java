640ea8421ee9447cd323f8479e8744d70a87a7fa
BosengJ <boseungee22@gmail.com>
2021-11-15 17:02:44 +0900

    test input
    input 문제 없는지 출력해서 확인하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,15 @@
 package racingcar;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Application {
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
 		String[] CarList = Game.generateCarList(scanner);
 		int playNum = Game.playGameNtimes(scanner);
-		//play the game n times
+		System.out.println(Arrays.toString(CarList));
+
 		// 기능 구현 목록
 		// Number of Game Plays
 		// Play Game (Print Result)
