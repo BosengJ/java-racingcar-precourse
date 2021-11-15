e5585f1ccd18a5e57d6ea9e1f4d2734e05ec1ebc
BosengJ <boseungee22@gmail.com>
2021-11-15 14:15:52 +0900

    Game.java - generateCarList 함수
    자동차 이름을 받아 자동차 리스트를 만들어주는 함수이다
    
    함수 안에 5자를 초과하면 반복할 수 있게 만들기

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,4 +1,21 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Game {
+	public static String[] generateCarList(Scanner scanner){
+		String[] CarList = new String[0];
+		boolean flag = true;
+		while(flag){
+			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+			String inputData = scanner.nextLine();
+			CarList = inputData.split(",");
+			// 받은 이름 5자 이하인지 확인하고 맞으면 플래그 바꿔주고 아니면 그대로 플래그 false
+			if(flag){
+				System.out.println("[ERROR] 자동차의 이름은 5자 이하여야 한다.");
+			}
+		}
+		return CarList;
+
+	}
 }
