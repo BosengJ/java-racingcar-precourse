1578ff90f4c438906faa4d307f8b285f609e2f9e
BosengJ <boseungee22@gmail.com>
2021-11-15 16:43:32 +0900

    application.java - playNum
    input 으로 게임 실행횟수를 받는다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,8 @@ public class Application {
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
 		String[] CarList = Game.generateCarList(scanner);
+		int playNum = Game.playGameNtimes(scanner);
+		//play the game n times
 		// 기능 구현 목록
 		// Number of Game Plays
 		// Play Game (Print Result)
